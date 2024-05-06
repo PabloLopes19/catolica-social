cbace5d510b0c6fdd55738d09314231729fe4071
PabloLopes19 <pabloelopes@hotmail.com>
2024-05-06 18:46:38 -0300

    Fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,14 +8,9 @@ import Post from "../components/post";
 import Users from "../fakeData/users.json";
 import Posts from "../fakeData/posts.json";
 import { useState } from "react";
-import { useSearchParams } from "next/navigation";
 
 export default function Dashboard() {
   const [posts] = useState<Post[]>(Posts);
-
-  const parmas = useSearchParams();
-
-  const username = parmas.get("username");
 
   return (
     <div>
@@ -25,9 +20,7 @@ export default function Dashboard() {
         <SideBar />
 
         <div className="flex w-full scrollbar-hide h-[calc(100vh-80px)] overflow-y-auto max-w-[1200px] flex-col">
-          <div className="flex w-full p-10 text-description">
-            Olá{username ? `, ${username}` : "!"}
-          </div>
+          <div className="flex w-full p-10 text-description">Olá!</div>
           {posts.map((post) => (
             <Post key={post.id} postData={post} />
           ))}
